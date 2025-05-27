9f5267e6fd40d722184c556f67b1e9c4145e4b2d
Zakaria MOUCHTATI <z.mouchtati@gmail.com>
2025-05-27 16:00:35 +0100

    change the port to 7000

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
-import express, { Request, Response } from "express"
+import express, { Request, Response } from "express";
 import cors from "cors";
-import chatbotRoute from "./routes/chatbot"
+import chatbotRoute from "./routes/chatbot";
 const app = express();
 app.use(express.json());
 app.use(cors());
 require("dotenv").config();
 
-
 app.use("/api", chatbotRoute);
 
-
-app.listen(4000 , () => {
-    console.log("App started in 4000 port")
-})
+app.listen(7000, () => {
+  console.log("App started in 4000 port");
+});
